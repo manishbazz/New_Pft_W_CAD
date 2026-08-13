ece927768e3b0e48106b4994dce045d975b947ca
manishbazz <bmanishsharma1718@gmail.com>
2026-08-13 15:19:22 +0530

    Add basePath and assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/New_Pft_W_CAD",
+  assetPrefix: "/New_Pft_W_CAD",
   images: {
     unoptimized: true,
   },
